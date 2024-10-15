a7b59f64d9060342c9d965ef31ba92467ed74c43
Mack Bakkum <mackbakkum@gmail.com>
2024-10-15 21:21:08 +0200

    implement: all components now have seperate HTML files for more clarity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,7 @@ import { RouterModule } from "@angular/router";
   selector: 'app-root',
   standalone: true,
   imports: [HomeComponent, RouterModule],
-  template: `
-    <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
-      </header>
-      <section class="content">
-        <router-outlet></router-outlet>
-      </section>
-    </main>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 
