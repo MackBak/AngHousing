b4952ba0a2d1a57c255151b0ce7534476accf507
Mack Bakkum <mackbakkum@gmail.com>
2024-10-15 22:29:02 +0200

    implement: form validation added
    - Added utility method for validation of the form

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,43 +1,54 @@
-import {Component, inject} from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from "@angular/router"; // TODO: Check what this is!
-import { HousingService } from "../housing.service";
-import { HousingLocation } from "../housing-location";
-import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ActivatedRoute} from "@angular/router"; // TODO: Check what this is!
+import {HousingService} from "../housing.service";
+import {HousingLocation} from "../housing-location";
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
-  selector: 'app-details',
-  standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
-  templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css']
+    selector: 'app-details',
+    standalone: true,
+    imports: [CommonModule, ReactiveFormsModule],
+    templateUrl: './details.component.html',
+    styleUrls: ['./details.component.css']
 })
 
-
 export class DetailsComponent {
-  route: ActivatedRoute = inject(ActivatedRoute);
-  housingService = inject(HousingService);
-  housingLocation: HousingLocation | undefined;
 
-  applyForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    email: new FormControl('')
-
-  })
-
-  constructor() {
-    const housingLocationId = Number(this.route.snapshot.params['id'])
-    this.housingService.getHousingLocationById(housingLocationId).then(housingLocation => {
-      this.housingLocation = housingLocation;
+    housingLocation: HousingLocation | undefined;
+    applyForm = this.fb.group({
+        firstName: ['', [Validators.required, Validators.maxLength(20)]],
+        lastName: ['', [Validators.required, Validators.maxLength(20)]],
+        email: ['', [Validators.required, Validators.email]],
     });
-  }
 
-  submitApplication() {
-    this.housingService.submitApplication(
-        this.applyForm.value.firstName ?? '',
-        this.applyForm.value.lastName ?? '',
-        this.applyForm.value.email ?? ''
-    );
-  }
+    constructor(private fb: FormBuilder, private route: ActivatedRoute, private housingService: HousingService) {
+        const housingLocationId = Number(this.route.snapshot.params['id']);
+        this.housingService.getHousingLocationById(housingLocationId).then(housingLocation => {
+            this.housingLocation = housingLocation
+        })
+    }
+
+    getErrorMessage(controlName: string): string | null {
+        const control = this.applyForm.get(controlName);
+
+        if (control?.hasError('required')) {
+            return 'This field is required!';
+        }
+        if (control?.hasError('maxlength') || control?.hasError('minlength')) {
+            return 'Name can only be between 3 and 20 characters!';
+        }
+        if (control?.hasError('email')) {
+            return 'Invalid email format!';
+        }
+
+        return null;
+    }
+
+    submitApplication() {
+        if (this.applyForm.valid) {
+            console.log('Form submitted', this.applyForm.value)
+        }
+    }
+
 }
