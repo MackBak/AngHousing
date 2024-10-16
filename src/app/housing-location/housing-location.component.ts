84b275d8c17df3ee0ca1136bb2c3c2e6d01909da
Mack Bakkum <mackbakkum@gmail.com>
2024-10-16 21:58:11 +0200

    fix: housing location component now has an ngOnInit to ensure proper error handling if no correct input is given.
    - Also seperated html to seperate file

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from "../housing-location";
 import { RouterModule } from "@angular/router";
@@ -7,21 +7,17 @@ import { RouterModule } from "@angular/router";
   selector: 'app-housing-location',
   standalone: true,
   imports: [CommonModule, RouterModule],
-  template: `
-    
-    <section class="listing">
-      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
-      <h2 class="listing-heading">{{housingLocation.name}}</h2>
-      <p class="listing-location">{{housingLocation.city}}, {{housingLocation.state}}</p>
-      <a [routerLink]="['/details', housingLocation.id]">Details</a>
-    </section>
-    
-  `,
+  templateUrl: './housing-location.component.html',
   styleUrls: ['./housing-location.component.css']
 })
 
 
-export class HousingLocationComponent {
+export class HousingLocationComponent implements OnInit {
   @Input() housingLocation!:HousingLocation;
 
+  ngOnInit() {
+    if (!this.housingLocation) {
+      console.warn('### Housing location input is missing! ###')
+    }
+  }
 }
